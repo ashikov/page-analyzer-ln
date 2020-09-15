aa49b1075c6525a141ef5faffbbfe420514b814a
Roman Ashikov <rmn.shkv@gmail.com>
2020-09-15 14:38:42 +0700

    add migration for domain_checks table

---
+++ database/migrations/2020_09_15_072737_create_domain_checks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDomainChecksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('domain_checks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('domain_id');
+            $table->foreign('domain_id')->references('id')->on('domains');
+            $table->integer('status_code');
+            $table->string('h1');
+            $table->string('keywords');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('domain_checks');
+    }
+}
