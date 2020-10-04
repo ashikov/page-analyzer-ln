c6b7aafe0cfacbccf8f1b13ffb9a8c49510e3013
Roman Ashikov <rmn.shkv@gmail.com>
2020-10-04 11:10:16 +0700

    fix domains.show view

--- resources/views/domains/show.blade.php
+++ resources/views/domains/show.blade.php
@@ -26,28 +26,28 @@
         {{ Form::open(['url' => route('domains.checks.store', $domain->id), 'method' => 'POST', 'class' => 'mb-3']) }}
             {{ Form::submit('Run check', ['class' => 'btn btn-primary']) }}
         {{ Form::close() }}
-        <table class="table table-bordered table-hover text-nowrap">
-            <tbody>
-                <tr>
-                    <th>{{ __('views.checks.id') }}</th>
-                    <th>{{ __('views.checks.status_code') }}</th>
-                    <th>{{ __('views.checks.h1') }}</th>
-                    <th>{{ __('views.checks.keywords') }}</th>
-                    <th>{{ __('views.checks.description') }}</th>
-                    <th>{{ __('views.checks.created_at') }}</th>
-                </tr>
-                @foreach ($checks as $check)
+            <table class="table table-bordered table-hover">
+                <tbody>
                     <tr>
-                        <td>{{ $check->id }}</td>
-                        <td>{{ $check->status_code }}</td>
-                        <td>{{ $check->h1 }}</td>
-                        <td>{{ $check->keywords }}</td>
-                        <td>{{ $check->description }}</td>
-                        <td>{{ $check->created_at }}</td>
+                        <th>{{ __('views.checks.id') }}</th>
+                        <th>{{ __('views.checks.status_code') }}</th>
+                        <th>{{ __('views.checks.h1') }}</th>
+                        <th>{{ __('views.checks.keywords') }}</th>
+                        <th>{{ __('views.checks.description') }}</th>
+                        <th>{{ __('views.checks.created_at') }}</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                    @foreach ($checks as $check)
+                        <tr>
+                            <td>{{ $check->id }}</td>
+                            <td>{{ $check->status_code }}</td>
+                            <td>{{ $check->h1 }}</td>
+                            <td>{{ $check->keywords }}</td>
+                            <td>{{ $check->description }}</td>
+                            <td>{{ $check->created_at }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
     </div>
 </div>
 @endsection
