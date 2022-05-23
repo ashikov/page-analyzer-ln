39dfb95843c00ea4558c5da75f5622b5d084ff43
Roman Ashikov <rmn.shkv@gmail.com>
2022-05-23 12:42:57 +0700

    add rollbar

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Rollbar\Laravel\RollbarServiceProvider::class
 
         /*
          * Package Service Providers...
